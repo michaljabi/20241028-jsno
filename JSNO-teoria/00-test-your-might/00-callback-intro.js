71d2e39bbae6a99e3a66150509f4d91cca7174a6
Jabi <michal.jabi@gmail.com>
2024-10-28 10:37:40 +0100

    task(00): how to run node js program + endless program

--- JSNO-teoria/00-test-your-might/00-callback-intro.js
+++ JSNO-teoria/00-test-your-might/00-callback-intro.js
@@ -11,3 +11,19 @@ function giveMeTheNumber(mistery) {
  * Poniżej odbierz liczbę 300 i pokaż ją na ekranie:
  * */
 
+let x = 1;
+
+function printNumber() {
+    console.log(x++)
+}
+
+
+// Program który będzie działał w nieskończoność
+setInterval(printNumber, 1000)
+
+// Najprostrze rozwiązanie:
+/*
+setInterval(() => {
+    console.log(x++)
+}, 1000)
+*/
