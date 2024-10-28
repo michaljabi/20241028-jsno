80250770648f54a972c0426eefd6fcaba2ab90f0
Jabi <michal.jabi@gmail.com>
2024-10-28 11:54:02 +0100

    solve(task-2):  make it right

--- JSNO-zadania/2-initial-project/main.js
+++ JSNO-zadania/2-initial-project/main.js
@@ -1,3 +1,19 @@
-console.log('****************************************');
-console.log('*   WITAJ W GENERATORZE BANNERÓW [!]   *');
-console.log('****************************************');
+// (2 podejście) robimy one line + jeden console.log ale tracimy na czytelności
+//console.log('****************************************\n*   WITAJ W GENERATORZE BANNERÓW [!]   *\n**************************************** ');
+
+//console.log(' " '); // one line string
+//console.log(" \" ' "); // one line string
+
+// Dobra praktyka: używaj code formattera.
+// Dla JS / TS jest to Prettier
+// extension: esbenp.prettier-vscode
+// dokumentacja: https://prettier.io/
+
+//console.log(" ? ");
+//console.log(" ? ");
+
+// 3 podejście: korzystamy z nowości języka
+console.log(`
+****************************************
+*   WITAJ W GENERATORZE BANNERÓW [!]   *
+****************************************`);
