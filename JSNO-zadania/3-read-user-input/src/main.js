b577736e43cdce83d3702c7cc5ac2b44dbeea7c7
Jabi <michal.jabi@gmail.com>
2024-10-28 15:09:09 +0100

    task(3 - complete): make it right

--- JSNO-zadania/3-read-user-input/src/main.js
+++ JSNO-zadania/3-read-user-input/src/main.js
@@ -1,18 +1,24 @@
-const mySentence = "To jest mój banner";
+const readline = require("readline");
+const read = readline.createInterface(process.stdin, process.stdout);
+
+console.log(`
+    ****************************************
+    *   WITAJ W GENERATORZE BANNERÓW [!]   *
+    ****************************************`);
+
+read.question("Podaj zdanie jakie chcesz zamienić w banner: ", (sentence) => {
+  const words = sentence.split(" ");
+  for (const word of words) {
+    makeBanner(word);
+  }
+  read.close();
+});
 
 // 1. zrobić duże litery
 // 2. podzielić na słowa
 // 3. iterować po tablicy (for...of)
 // 4. dodać * patrz: repeat() dla stringa + .length
 // 5. wypisać na ekranie wszystkie bannery
-
-const words = mySentence.split(" ");
-
-// words; //=
-// const PADDING_LENGTH = 4;
-for (const word of words) {
-  makeBanner(word);
-}
 
 function makeBanner(word) {
   const insideWord = `* ${word.toUpperCase()} *`;
@@ -30,7 +36,7 @@ function makeBanner(word) {
 //   console.log("*".repeat(word.length + 4));
 // });
 
-makeBanner("Michał");
+// makeBanner("Michał");
 
 // words; //=
 // words.map(makeBanner); //=
