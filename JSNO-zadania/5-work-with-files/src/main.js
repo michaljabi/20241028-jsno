7b0c26835e4be44f31adddd1ed33d76738838674
Jabi <michal.jabi@gmail.com>
2024-10-29 12:42:17 +0100

    task(5 - zadania): read + write to file. make it work

---
+++ JSNO-zadania/5-work-with-files/src/main.js
@@ -0,0 +1,56 @@
+import path from "node:path";
+import readline from "node:readline";
+import process from "node:process";
+import { readFile, writeFile } from "node:fs";
+import { makeDirname } from "./utils/make-dirname.js";
+
+const dataPath = path.resolve(
+  makeDirname(import.meta.url),
+  "files",
+  "data.txt"
+);
+const read = readline.createInterface(process.stdin, process.stdout);
+
+// odczyt pliku
+readFile(dataPath, "utf-8", (err, data) => {
+  if (err) {
+    if (err.code === "ENOENT") {
+      console.log("Plik data nie ma danych...");
+      return askAboutDataToAdd();
+    }
+    console.log("Problem z odczytem pliku");
+    read.close();
+    process.exit(1);
+  }
+  console.log("Dane z pliku:");
+  console.log(data);
+  console.log("-------------");
+  askAboutDataToAdd();
+});
+
+function askAboutDataToAdd() {
+  read.question("Czy chcesz coś dopisać na koniec pliku (y/n): ", (letter) => {
+    if (letter.toLowerCase() !== "y") {
+      read.close();
+      process.exit();
+    }
+    getDataFromUser();
+  });
+}
+
+function getDataFromUser() {
+  read.question("Wpisz dane do pliku: ", (dataToAdd) => {
+    writeFile(
+      dataPath,
+      dataToAdd + "\n",
+      { encoding: "utf-8", flag: "a" },
+      (err) => {
+        if (err) {
+          console.error(err.message);
+        }
+        read.close();
+        process.exit();
+      }
+    );
+  });
+}
