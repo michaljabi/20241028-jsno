fac9c319ac2cfb220d90847ce05f9e0f70eaf5bb
Jabi <michal.jabi@gmail.com>
2024-10-29 10:52:56 +0100

    task(04 - zadania): Make it work with whole logic.

--- JSNO-zadania/4-modular-application/src/main.js
+++ JSNO-zadania/4-modular-application/src/main.js
@@ -6,10 +6,34 @@ const read = readline.createInterface(process.stdin, process.stdout);
 
 makeProgramTitle();
 
-read.question("Podaj zdanie jakie chcesz zamienić w banner: ", (sentence) => {
-  const words = sentence.split(" ");
-  for (const word of words) {
-    makeBanner(word);
-  }
-  read.close();
-});
+function askAboutBanner() {
+  read.question("Podaj zdanie jakie chcesz zamienić w banner: ", (sentence) => {
+    const words = sentence.split(" ");
+    for (const word of words) {
+      makeBanner(word);
+    }
+    askWhatNext();
+  });
+}
+
+function askWhatNext() {
+  read.question(
+    `Co robimy dalej - wpisz odpowiednią literę:
+[k]oniec
+[n]astępny banner
+`,
+    (letter) => {
+      if (letter.toLowerCase() === "k") {
+        read.close();
+        process.exit();
+      } else if (letter.toLowerCase() === "n") {
+        askAboutBanner();
+      } else {
+        console.log(`Nieobsługiwany wybór "${letter}"!  `);
+        askWhatNext();
+      }
+    }
+  );
+}
+
+askAboutBanner();
