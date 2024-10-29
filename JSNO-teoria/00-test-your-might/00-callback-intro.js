1cb2dd9481f00e4274dc443af8b2a45be8de7f51
Jabi <michal.jabi@gmail.com>
2024-10-29 09:32:29 +0100

    task(00): warm up. callback used to recieve 300

--- JSNO-teoria/00-test-your-might/00-callback-intro.js
+++ JSNO-teoria/00-test-your-might/00-callback-intro.js
@@ -1,8 +1,17 @@
 
 console.log('Hello stranger... shall we start?')
 
+// function giveMeTheNumber(mistery = (a = 0) => { }) {
+// PROVIDER:
 function giveMeTheNumber(mistery) {
-    mistery(300)
+    // console.log(mistery);
+    // undefined(300)
+    // mistery(500)
+
+    setTimeout(() => {
+        // mistery(300)
+
+    }, 4000)
 }
 
 /**
@@ -10,6 +19,59 @@ function giveMeTheNumber(mistery) {
  *
  * Poniżej odbierz liczbę 300 i pokaż ją na ekranie:
  * */
+// CONSUMER #1
+giveMeTheNumber((num) => {
+    console.log(num);
+})
+
+// CONSUMER #2
+giveMeTheNumber((num) => {
+    console.log(num);
+})
+
+// CONSUMER #3
+giveMeTheNumber((num) => {
+    console.log(num);
+})
+
+// CONSUMER #4
+giveMeTheNumber((num) => {
+    console.log(num);
+})
+
+
+function addTwoNumbers(a = 0, b = 0) {
+    return a + b;
+}
+
+
+console.log(undefined + undefined)
+addTwoNumbers() //=
+
+
+const result = addTwoNumbers(10, 20)
+
+console.log(result);
+
+
+console.log(addTwoNumbers('!', 'hello'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Inne przykłady:
+
 
 let x = 1;
 
