3b0c48a116c081887366cd3b7e15930e3b9d89a4
Jabi <michal.jabi@gmail.com>
2024-10-30 16:32:17 +0100

    explain: SQL migration by example of adding "name" to user table.

---
+++ JSNO-bazy-danych/databases-sql/knex-mysql/db/migrations/20241030150907_user_should_have_name.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+    return knex.schema.alterTable('users', (table) => {
+		table.string('name')
+	})
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+    return knex.schema.alterTable('users', (table) => {
+		table.dropColumn('name')
+	})
+};
