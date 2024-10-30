7cceba200060ce57aebd15f9553fc3e4f8657b51
Jabi <michal.jabi@gmail.com>
2024-10-30 09:35:36 +0100

    task(00.01): error handling.

--- JSNO-teoria/00-test-your-might/01-code-with-error-handling.js
+++ JSNO-teoria/00-test-your-might/01-code-with-error-handling.js
@@ -3,4 +3,32 @@
  *
  * Zrób funkcję, która dzieli 2 liczby
  * i rzuca błąd "Cannot divide by 0" jeśli 2 liczba to "0"
+ * 
+ * zobacz: przykład b301.
  * */
+
+
+function divide(a, b = 1) {
+    if (b === 0) {
+        throw new Error('Cannot divide by 0')
+    }
+    return a / b;
+}
+
+
+
+try {
+    divide(10, 2) //=
+    divide(10) //=
+    divide(10, 0) //=
+
+    console.log('Unreachable console.log')
+} catch (e) {
+    console.log(e.message)
+} finally {
+    console.log('I run always...')
+}
+
+
+console.log('Unreachable console.log !');
+
